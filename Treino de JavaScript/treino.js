7f6ce77dc8c38a1fc705d34ab4ca570c4f14f729
ROSANA ALVES FREIRE ALMEIDA <rosana.almeida@upbrasil.com>
2023-03-14 12:14:03 -0300

    Aprendendo JavaScript 14/03

---
+++ Treino de JavaScript/treino.js
@@ -0,0 +1,128 @@
+// Início do treino de JavaScript
+
+console.log('Teste')
+
+// 1 - Váriaveis:
+
+    // let nome = 'Marcos';' {STRING LITERAL}'
+    // let idade = 18; '{NUMBER LITERAL}'
+    // let altura = 1.65;
+    // console.log(nome,idade,altura)
+
+// 2 - Valor das váriaveis 'let' podem sempre mudar:
+
+    // let valorQuePodeMudar = 10;
+    // valorQuePodeMudar = 20;
+    // console.log(valorQuePodeMudar)
+
+// 3 - Constantes, valor constante que não precise de mudança, use const em geral, a não ser que precise fazer mudanças:
+
+    // const valorQueNãoPodeMudar = 10;
+    // valorQueNãoPodeMudar = 20; '{ERROR}'
+ 
+// 4 - Tipos Primitivos; Tipos de Referência e Tipos de Valores
+
+// 5 - TIPOS DE VALOR
+
+    // let nome = 'Marcos'; '{STRING LITERAL}'
+    // let idade = 18; '{NUMBER LITERAL}'
+    // let maiorDeIdade = True; '{Boolean}'
+    // let sobrenome = undefined; '{Undefined - Pouco Utilizado}'
+    // let corSelecionada = null; '{Redefinir um valor - Ex: Alterar de fundo} [Object]'
+
+// 6 - Tipagem Dinâmica
+
+    // typeof nome {Comando no CONSOLE para descobrir qual a tipagem de uma váriavel}
+    // 'string' {Tipagem do váriavel "nome" utilizado no ultimo 'exercício'}
+
+// 7 - TIPOS DE REFERÊNCIA - [Objetos/Arrays/Functions]
+// 7.1 - OBJETOS [Classes]
+
+//  keyValour [key = valor] - Dentro da Classe
+    //  let pessoa = {
+    //     nome: 'Marcos',
+    //     idade: 18,
+    //     maiorDeIdade: true,
+    //     sobrenome: 'paulo'
+    // };
+    // console.log(pessoa)
+
+// 7.2 - ARRAYS
+
+    // let family = ['Marcos',18,'Gustavo',19,true];
+    // console.log(family); - 'TODOS AS VÁRIAVEIS'
+    // console.log(family[0]); - 'Váriavel em Ordem, (0)'
+    // console.log(family[2]); - 'Váriavel em Ordem, (2)'
+    // console.log(family.length); - 'Identificar o tamanho do conjunto, =(5)'
+
+// 7.3 - FUNCTIONS
+
+// Nome funções = Verbo(ação) + Substantivo(O que faz essa ação)
+// Ex: resetarCor, alterarImagem, alterarTamanho
+
+    // let corSite = 'azul';
+    // function resetarCor(){
+    //     corSite = '';
+    // }
+    // console.log(corSite)
+    // resetarCor();
+    // console.log(corSite);
+
+ // ADICIONANDO PARÂMETROS ÀS FUNÇÕES (Alterar)
+
+    // let corSite = 'azul';
+    // let corFolha = 'Branca;'
+    // function resetarCor(cor){
+    //     corSite = cor;
+    //     corFolha = cor;
+    // }
+    // console.log(corSite,corFolha);
+    // resetarCor('vermelho');
+    // console.log(corSite,corFolha);
+
+// ADICIONANDO DOIS PARÂMETROS
+
+    // let corSite = 'azul';
+    // let corFolha = 'Branca;'
+    // function resetarCor(cor,tonalidade){
+    //     corSite = cor + ' ' + tonalidade;
+    //     corFolha = cor;
+    // }
+    // console.log(corSite,corFolha);
+    // resetarCor('vermelho','escuro');
+    // console.log(corSite,corFolha);
+
+// 8 - TIPOS DE FUNÇÕES
+// 8.1 - Realizar uma tarefa, não devolve nada('SEM RETORNO')
+    // function dizernome(){
+    //     console.log('Marcos');
+    // }
+    // dizernome(); - 'Dentro da função, o objetivo disso foi apenas transmitir/dizer esse nome'
+// 8.2 - Realizar um cálculo ou operação com retorno
+    // function multiplicarPorTres(valor){
+    //     return valor* 3;
+    // }
+    // // console.log (multiplicarPorTres(10)) - 'ex: 1'
+    // resultado = multiplicarPorTres(20);
+    // console.log(resultado)
+// 9 - OPERADORES
+    // - Operadores Aritiméticos (matemáticos)
+    // - Operadores de Atribuição
+    // - Operadores de Comparação
+    // - Operadores Lógicos
+    // - Operadores Bitwise
+// 9.1 - 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
