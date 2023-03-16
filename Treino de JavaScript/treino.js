e1eb0aef07b1dfd568d6234061b05d837a3e23a0
ROSANA ALVES FREIRE ALMEIDA <rosana.almeida@upbrasil.com>
2023-03-15 22:15:04 -0300

    15/03

--- Treino de JavaScript/treino.js
+++ Treino de JavaScript/treino.js
@@ -111,18 +111,284 @@ console.log('Teste')
     // - Operadores de Comparação
     // - Operadores Lógicos
     // - Operadores Bitwise
-// 9.1 - 
-
+// 9.1 - Operadores Aritiméticos
+// (+),(-),(*),(/),(**) - Cálculos
+    // let salario = 1400;
+    // console.log(salario + salario);
+    // console.log(salario - salario);
+    // console.log(salario * salario); 
+    // console.log(salario / salario); 
+    // console.log(5 ** 5); 
+// (++),(--) - Incrementando/Decrementando
+    // let idade = 18;
+    // console.log(idade++);
+    // console.log(idade); - '19'
+    // console.log(idade--);
+    // console.log(idade); - '17'
+    // console.log(++idade); - '19'
+    // console.log(--idade); - '17'
+// 9.2 - Operadores de Atribuição
+    // valorPcGamer = 5000;
+    // valorPcGamer += valorPcGamer; - 'Isso seria a mesma coisa de valorPcGamer = valorPcGamer + valorPcGamer;'
+    // console.log(valorPcGamer);
+    // valorPcGamer -= valorPcGamer; - 'Isso seria a mesma coisa de valorPcGamer = valorPcGamer - valorPcGamer;'
+    // console.log(valorPcGamer);
+// 9.3 - Operadores de Igualdade (Comparação)
+// Igualdade estrita - (Mais Recomendado)
+//     console.log( 1 === 1); '(True)'
+//     console.log('1' === 1); '(False)'
+// Igualdade solta - (Menos Recomendado devido a sua complexidade e variações!)
+//     console.log(1 == 1); '(True)'
+//     console.log('1' == 1); '(True)'
+// 9.4 - Operador Ternário
+    // MD10 ranked, se tiver jogado 10 partidas >= com rank : sem rank;
+    // (let 'atributo' = 'atributocomparado' '+-=/*' 'valorcomparado' '?' 'True' : 'False';)
+        // let partidas = 11
+        // let rank = partidas >= 10 ? 'com rank' : 'sem rank';
+        // console.log(rank);
+// 9.5 - Operadores Lógicos
+    // Operador Lógico 'e/and' (&&)
+    // Retorna True se todos os operando forem true
+        // let maiorDeIdade = true;
+        // let carteiraDeTrabalho = true;
+        // let podeAplicar = maiorDeIdade && carteiraDeTrabalho;
+        // console.log(podeAplicar); 'Todas condições devem ser true, caso contrario, resultado será false'
+            // let maiorDeIdade = false;
+            // let carteiraDeTrabalho = true;
+            // let serInteligente =  true;
+            // let podeAplicar = maiorDeIdade && carteiraDeTrabalho && serInteligente;
+            // console.log(podeAplicar); 
+    // Operador Lógico 'ou/or' (||)
+        // let maiorDeIdade = false;
+        // let carteiraDeTrabalho = true;
+        // let podeAplicar = maiorDeIdade || carteiraDeTrabalho;
+        // console.log(podeAplicar); 'Apenas uma das condições precisam ser true para o resultado ser true'
+        // 'Caso nenhuma delas forem true, ou seja, todas serem falsas, o resultado também será falso'
+    // Junção de Operadores Lógicos (&& e ||)
+            // let maiorDeIdade = false;
+            // let carteiraDeTrabalho = false;
+            // let serInteligente =  true;
+            // let podeAplicar = maiorDeIdade || carteiraDeTrabalho && serInteligente;
+            // console.log(podeAplicar);
+    // Operador Lógico NOT (!) 
+            // let maiorDeIdade = false;
+            // let carteiraDeTrabalho = false;
+            // let serInteligente =  true;
+            // let podeAplicar = maiorDeIdade || carteiraDeTrabalho && serInteligente;
+            // console.log(podeAplicar);
+            // let candidatoResucado = !podeAplicar;    'candidatoRecusado =(CASO) !(NOT/NÃO)podeAplicar;'
+            // console.log(candidatoResucado);
+                // 'MEU EXEMPLO:'
+                // let dominarHTML = true;
+                // let dominarCSS = true;
+                // let dominarJS = false;
+                // let qualificadoSerProgramador = dominarHTML && dominarCSS && dominarJS;
+                // console.log(qualificadoSerProgramador);
+                // let nãopoderTrabalharComoDev = !qualificadoSerProgramador;
+                // console.log(nãopoderTrabalharComoDev);
+// 9.6 - Comparações não boleanos
+    // Falsy 
+        // undefined
+        // null
+        // false
+        // 0
+        // ' '
+        // NaN Not a Number (Valor inválido em cálculos matemáticos)
+    // Truthy
+        // Qualquer valor ou atributo que não seja um Falsy
+        // Ex: false || 'rafael' ('rafael')
+        // Ex2: false || 1 (1)
+        // Ex3: false || 1 || 3 (1) - { (||) ou/or sempre vai retornar o primeiro valor verdadeiro e ignorar o resto.}
+        // let corPersonalizada = 'vermelho';
+        // let corPadrao = 'azul';
+        // let corPerfil = corPersonalizada || corPadrao;
+        // console.log(corPerfil);
+// 9.7 - Invertendo valores finais
+    // let a = 'vermelho';
+    // let b = 'azul';
     
+    // let c = a;
+    // a = b;
+    // b = c;
+
+    // console.log(a);
+    // console.log(b);
+// 10 - If..Else
+    // Se a hora estiver entre 06:00 até 12:00 será 'Bom Dia', se a hora estiver entre 12:00 até 18:00 será
+    // 'Boa Tarde', caso contrario, será Boa Noite.
+    // let hora = 7;
+    //  if(hora > 6 && hora < 12) {
+    //     console.log('Bom Dia');
+    //  }
+    // else if(hora >12 && hora <18) {
+    //     console.log('Boa Tarde');
+    // }
+    // else {
+    //     console.log('Boa Noite')
+    // }
+// 11 - Switch..Case
+    // let permissao = 'adm'
+    //     switch (permissao){
+    //         case 'usuario':
+    //             console.log('Bom Dia Usuário');
+    //         break
+    //         case 'desenvolvedor':
+    //             console.log('Bom Dia Criador');
+    //             break
+    //         case 'adm':
+    //             console.log('Bom Dia Administrador!')
+    //         break
+    //         default: console.log('Usuário não identificado')
+    //     }
+// 12 - Loop/Laço
+    // For
+    // While
+    // Do.. while
+    // For.. in
+    // For..off
+// For
+        // for(let i = 1; i <=7; i++) {
+        //     console.log('EU VÁRIAS VEZES',i);
+        // }
+        // for(let i = 1; i <8; i++){
+        //    if(i % 2 !==0) {
+        //     console.log('ESTOU BIRUTA', i);
+        //     }
+        // }
+            // CUIDADO, LOOP INFITO KKK
+            // for(let i = 20; i >8; i++){
+            //     if(i % 2 !==0) {
+            //      console.log('ESTOU BIRUTA', i);
+            //      }
+// While loop
+    // let i = 5;
+    // while(i >=5){
+    //     if(i % 2 !==0){
+    //         console.log('Biruta Maximo',i)
+    //     }--i;
+// Do..While
+    // let i = 2
+    //     do{
+    //         console.log('NÃO CAIA EM UM LOOPING PFV', i); 
+    //     i++;
+    //     } while(i<=10)
+// Loop For..in
+    // const pessoa = {
+    //     nome: 'Marcos' ,
+    //     idade: 18 , 
+    //     altura: 1.60
+    // };
+    
+    // for(let chave in pessoa){
+    //     console.log(chave, pessoa.nome)
+    // }
+    // const cores = ['Vermelho', 'Azul' , 'Rosa']
+    
+    // for(let indice in cores)
+    // console.log(indice, cores[indice])
+// Loop For..of
+    // const cores = ['Rosa','Roxo','Preto','Branco'];
+    // for(let cor of cores){
+    //     console.log(cor)
+    // }
+// Retorne o valor maior
+    // let oValor = valorMaior(15,9);
+    // console.log(oValor);
+    // function valorMaior(numero1, numero2){
+    //     if(numero1 > numero2){
+    //   return numero1
+    // }
+    // else return numero2
+    // }
+    
+    
+    // let oValor = valorMaior(15,9);
+    // console.log(oValor);
+    // function valorMaior(numero1, numero2){
+    //   return numero1 > numero2? numero1 : numero2;
+    // }
+// FizzBuzz
+        // Divisível por 3 => Fizz
+        // Divisível por 5 => Buzz
+        // Divisível por 3 e 5 => FizzBuzz
+        // Não é divisível por 3 e 5 => entrada
+        // Não é um número => 'não é um número'
+            // const resultado = fizzbuzz('oi');
+            // console.log(resultado);
+
+            // function fizzbuzz(entrada){
+            
+            //     if(typeof entrada !=='number')
+            //         return 'não é um número'
+            //     if(entrada % 3 ===0 && entrada % 5 ===0)
+            //         return 'FizzBuzz';
+            //     if(entrada % 3 !==0 && entrada % 5 !==0)
+            //         return entrada;          
+            //     if(entrada % 3 ===0)
+            //         return 'Fizz';
+            //     if(entrada % 5 ===0)
+            //     return 'Buzz';
+            // }
+// Medidor de velocidade
+    // Velocidade maxima de até 70km
+    // a cada 5km acima do limite você ganha 1 ponto
+    // Math.Floor()
+    // caso pontos maior que 12 -> Carteira Suspendida
+        // verificadorDeVelocidade(1000);
+
+        // function verificadorDeVelocidade(velocidade){
+        //     if(velocidade <= 70 )
+        //     console.log('ok');
+        //     if(Math.floor((velocidade - 70) / 5 )>=12)
+            
+        //     console.log('Carteira Suspendida');
+        //     else if (velocidade >70)
+        //     console.log('Pontos:'+ Math.floor(((velocidade - 70) / 5)));
+        // }
+    // ATRIBUINDO CONSTANTES E LIMPANDO/SIMPLIFICANDO O CÓDIGO
+        //     verificadorDeVelocidade(129);
+
+        //     function verificadorDeVelocidade(velocidade){
+        //         const velocidadeMaxima = 70;
+        //         const kmPorPonto = 5
+        //         const pontos = Math.floor(((velocidade - velocidadeMaxima) / kmPorPonto))
+        //         if(velocidade <= velocidadeMaxima )
+        //         console.log('ok');
+        //         if(pontos >= 12)
+        //         console.log('Carteira Suspendida');
+        //         else if (velocidade >velocidadeMaxima)
+        //         console.log('Pontos:'+ pontos);
+        // }
+//Par ou Impar
+    // Receber uma quantidade de valores para avaliar
+    // função exibe se cada valor é impar ou par
+        // exibirTipo(5);
+        // function exibirTipo(limite){
+        //    let i = 1;
+        //    while(i <=limite){
+        //         a = i
+        //         i = limite
+        //     if(i % 2 !==0){
+        //         console.log(i + 'IMPAR');
+        //     }
+        //     i = a
+        // }++i;
+        //     while(i <=limite){
+        //         b = i
+        //         i = limite
+        //      if(i % 2 ===0){
+        //         console.log(i + 'PAR');
+        //     }
+        //     i = b
+        //     }++i;
+        // }
 
 
 
 
 
-
-
-
-
-
-
-
+// let i = 5;
+    // while(i >=5){
+    //     if(i % 2 !==0){
+    //         console.log('Biruta Maximo',i)
+    //     }--i;
